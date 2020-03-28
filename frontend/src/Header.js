c3f01aec2cad3072b37f0851847602bfff135bea
jeandobre <jeandobre@ufgd.edu.br>
2020-03-28 14:20:50 -0400

    Criando o front-end tela de logon e tela de registro

---
+++ frontend/src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header({ title, children }){
+	return (
+		<header>
+			<h1>{children}</h1>
+			<span>{title}</span>
+		</header>
+	);
+}
+
+export default Header;
