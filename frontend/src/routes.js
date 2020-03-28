24737c1ddb20829693d9aa57fc333888600d15f4
jeandobre <jeandobre@ufgd.edu.br>
2020-03-28 16:50:33 -0400

    Criando pagina novo  incidentes e perfil

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Logon from "./pages/Logon";
 import Register from "./pages/Registro";
+import Profile from "./pages/Perfil";
+import NewIncident from "./pages/NovoIncidente";
 
 export default function Routes() {
 	return (
@@ -10,6 +12,8 @@ export default function Routes() {
 			<Switch>
 				<Route path="/" exact component={Logon} />
 				<Route path="/registro" component={Register} />
+				<Route path="/perfil" component={Profile} />
+				<Route path="/incidentes/novo" component={NewIncident} />
 			</Switch>
 		</BrowserRouter>
 	);
